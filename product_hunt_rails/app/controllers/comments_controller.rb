2b378f7253dc5fd9616dc0e57b371928f6d1be70
Tom Luce <thomasgluce@gmail.com>
2015-07-17 10:04:34 +0100

    Modified comments index action

--- product_hunt_rails/app/controllers/comments_controller.rb
+++ product_hunt_rails/app/controllers/comments_controller.rb
@@ -18,24 +18,25 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to post_comments_path(@post), notice: 'Comment was successfully created.'
     else
-      render :new
+      @comments = @post.comments
+      render :index
     end
   end
 
   private
 
-    def set_post
-      @post = Post.find(params[:post_id])
-    end
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
 
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = @post.comments.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_comment
+    @comment = @post.comments.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def comment_params
-      params.require(:comment).permit(:content)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 
 end
